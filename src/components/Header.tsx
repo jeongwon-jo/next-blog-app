e299895ab7e6c7ea637c700e2b91de0444589721
jeongwon-jo <jjeong1.jo@gmail.com>
2026-04-14 17:11:31 +0900

    공통 IconButton 컴포넌트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { FC } from "react"
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
 import { BsRobot } from "react-icons/bs"
+import IconButton from "./IconButton"
 
 type HeaderProps = {
   isSidebarOpen: boolean;
@@ -11,21 +12,16 @@ type HeaderProps = {
 const Header: FC<HeaderProps> = ({isSidebarOpen, setIsSidebarOpen}) => {
   return (
     <header className="flex h-16 items-center justify-between border-b px-4 lg:h-20 lg:px-10">
-      <button className="p-2" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-        {isSidebarOpen ? (
-          <AiOutlineClose className="size-5 lg:size-6" />
-        ) : (
-          <AiOutlineMenu className="size-5 lg:size-6" />
-        )}
-      </button>
+      <IconButton
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        Icon={isSidebarOpen ? AiOutlineClose : AiOutlineMenu}
+      />
       <Link href={'/'}>
         <h1 className="text-3xl font-semibold text-slate-600 lg:text-4xl">
           JEONG1 LOG
         </h1>
       </Link>
-      <Link href={'/search'}>
-        <BsRobot className="size-5 lg:size-6" />
-      </Link>
+      <IconButton Icon={BsRobot} component={Link} href={'/search'} />
     </header>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/utils/style";
 import Link from "next/link";
 import { FC } from "react";
 import { AiFillGithub, AiFillInstagram, AiOutlineClose } from "react-icons/ai";
+import IconButton from "./IconButton";
 
 type SidebarProps = {
   close: () => void;
@@ -10,9 +11,14 @@ type SidebarProps = {
 
 const Sidebar:FC<SidebarProps> = ({close, isOpen}) => {
   return (
-    <div className={cn('absolute min-h-screen flex-col gap-6 border-r bg-white px-6 py-10 text-base lg:relative', isOpen ? 'flex' : 'hidden')}>
+    <div
+      className={cn(
+        'absolute min-h-screen flex-col gap-6 border-r bg-white px-6 py-10 text-base lg:relative',
+        isOpen ? 'flex' : 'hidden',
+      )}
+    >
       <div className="flex justify-end lg:hidden">
-        <AiOutlineClose className="size-6" onClick={close} />
+        <IconButton Icon={AiOutlineClose} onClick={close} />
       </div>
       <Link
         href={'/'}
@@ -34,12 +40,8 @@ const Sidebar:FC<SidebarProps> = ({close, isOpen}) => {
       </Link>
 
       <div className="mt-10 flex items-center gap-4">
-        <Link href={''}>
-          <AiFillInstagram className="size-6" />
-        </Link>
-        <Link href={''}>
-          <AiFillGithub className="size-6" />
-        </Link>
+        <IconButton Icon={AiFillInstagram} component={Link} href={''} />
+        <IconButton Icon={AiFillGithub} component={Link} href={''} />
       </div>
     </div>
   );
